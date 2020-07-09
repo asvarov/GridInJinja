b82fea76202fd22147ebe45e2dcf477692b9b9f7
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-07-09 10:52:44 +0300

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    elements = [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+        'Sixth',
+        'Seventh',
+        'Eighth',
+        'Ninth',
+        'Tenth',
+        'Eleventh',
+        'Twelfth'
+    ]
+    return flask.render_template('index.html', elements=elements*2)
+
+
+if __name__ == '__main__':
+    app.run()
+
